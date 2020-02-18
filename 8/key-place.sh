dc5ced61953aed075c00bd0b41ae4e778c947b1b
Shawn Duncan <sduncan@digitalpulp.com>
2020-02-18 15:31:34 -0500

    Set /.ssh/config to add new hosts

--- 8/key-place.sh
+++ 8/key-place.sh
@@ -3,12 +3,10 @@ if [ ! -d "/root/.ssh" ]; then
   mkdir /root/.ssh
 fi
 if [ -n "${GIT_KNOWN_HOST}" ]; then
-  echo -e ${GIT_KNOWN_HOST} >> ${HOME}/.ssh/known_hosts
-  ssh-keyscan -H bitbucket.org >> /root/.ssh/known_hosts
-  ssh-keyscan -H github.com >> /root/.ssh/known_hosts
+  echo -e ${GIT_KNOWN_HOST} >> /root/.ssh/known_hosts
 fi
 if [ -n "${SSH_PRIVATE_KEY}" ]; then
-  echo -e ${SSH_PRIVATE_KEY} >> ${HOME}/.ssh/id_rsa
+  echo -e ${SSH_PRIVATE_KEY} >> /root/.ssh/id_rsa
 fi
 chmod 0700 /root/.ssh/known_hosts
 chmod 0600 /root/.ssh/id_rsa
