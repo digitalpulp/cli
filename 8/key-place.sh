e88d2395f1251fc7b67b6be13df956a4c65c8632
Shawn Duncan <sduncan@digitalpulp.com>
2020-02-18 14:44:17 -0500

    Add bitbucket & github key-scan

--- 8/key-place.sh
+++ 8/key-place.sh
@@ -4,6 +4,8 @@ if [ ! -d "/root/.ssh" ]; then
 fi
 if [ -n "${GIT_KNOWN_HOST}" ]; then
   echo -e ${GIT_KNOWN_HOST} >> ${HOME}/.ssh/known_hosts
+  ssh-keyscan -H bitbucket.org >> /root/.ssh/known_hosts
+  ssh-keyscan -H github.com >> /root/.ssh/known_hosts
 fi
 if [ -n "${SSH_PRIVATE_KEY}" ]; then
   echo -e ${SSH_PRIVATE_KEY} >> ${HOME}/.ssh/id_rsa
